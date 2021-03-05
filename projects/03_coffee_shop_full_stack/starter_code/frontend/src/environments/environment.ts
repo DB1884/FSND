fdd6e4a62345bbb943bacf24c52475271534022c
David Byrne <davebyr@gmail.com>
2021-03-05 11:46:40 +0000

    Complete coffee shop task

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -1,14 +1,10 @@
-/* @TODO replace with your variables
- * ensure all variables on this page match your project
- */
-
 export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'fsnd-coffeeshopdb.eu', // the auth0 domain prefix
+    audience: 'localhost:5000', // the audience set for the auth0 app
+    clientId: 'IVkNVPecjE5bterc9uXFVa8dAkwdELbY', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
